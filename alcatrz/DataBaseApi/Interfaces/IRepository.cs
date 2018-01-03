ee55b900db03186ace04af4d4878b38c88f254e9
00029244 <00029244@DFS-00029244>
2018-01-03 10:48:19 +0600

    done

---
+++ alcatrz/DataBaseApi/Interfaces/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataBaseApi.Interfaces
+{
+    public interface IRepository<T> : IDisposable
+        where T : class
+    {
+        T GetItems(int id);
+        IEnumerable<T> GetItemList();
+        void Create(T item); // создание объекта
+        void Update(T item); // обновление объекта
+        void Delete(int id); // удаление объекта по id
+        void Save();  // сохранение изменений
+    }
+}
